2657977e85f359495dd6635a6065039eb4301145
Yvan <ynmartin@gmail.com>
2017-08-14 22:17:58 +1000

    Initial commit

---
+++ src/ymartin/traffic/Main.java
@@ -0,0 +1,7 @@
+package ymartin.traffic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ test/ymartin/traffic/MainTest.java
@@ -0,0 +1,7 @@
+package ymartin.traffic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+}
